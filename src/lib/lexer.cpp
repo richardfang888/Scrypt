620213125934af249b0049bb28abdf763d6a9007
Aaron Drumm <adrumm@cs.ucsb.edu>
2023-10-16 16:52:27 -0700

    Parse fixing

---
+++ src/lib/lexer.cpp
@@ -0,0 +1,131 @@
+# include "lex.hpp"
+# include <iomanip>
+# include <iostream>
+# include <algorithm>
+
+using namespace std;
+
+// Helper function to create and push tokens
+void createToken(Token &currToken, vector<Token> &tokens, TokenType type, const string &text, int length) {
+    currToken.type = type;
+    currToken.text = text;
+    currToken.length = length;
+    tokens.push_back(currToken);
+    currToken.columnNumber += length;
+}
+
+vector<Token> readTokens(string &input) {
+    vector<Token> tokens;
+    Token currToken;
+    currToken.type = OTHER;
+    currToken.lineNumber = 1;
+    currToken.columnNumber = 1;
+
+    for (char c : input) {
+        if (currToken.type == DOT && !(c >= '0' && c <= '9')) { 
+            createToken(currToken, tokens, OTHER, "error", 1);
+            return tokens;
+        }
+
+        // handle delimiters
+        switch (c) {
+            case '\n':
+                currToken.type = NEWLINE;
+                currToken.lineNumber++;
+                currToken.columnNumber = 1;
+                break;
+            case ' ':
+            case '\t':
+                currToken.type = WHITESPACE;
+                currToken.columnNumber++;
+                break;
+            case '(':
+                createToken(currToken, tokens, LEFT_PAREN, "(", 1);
+                break;
+            case ')':
+                createToken(currToken, tokens, RIGHT_PAREN, ")", 1);
+                break;
+
+            // handle operators
+            case '+':
+                createToken(currToken, tokens, PLUS, "+", 1);
+                break;
+            case '-':
+                createToken(currToken, tokens, MINUS, "-", 1);
+                break;
+            case '*':
+                createToken(currToken, tokens, TIMES, "*", 1);
+                break;
+            case '/':
+                createToken(currToken, tokens, DIVIDES, "/", 1);
+                break;
+            
+            // handle numbers
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                if (currToken.type == FLOAT || currToken.type == DOT) {
+                    currToken.text += c;
+                    currToken.length++;
+                    currToken.columnNumber = tokens.back().columnNumber; 
+                    tokens.pop_back();
+                    createToken(currToken, tokens, FLOAT, currToken.text, currToken.length);
+                }
+                else {
+                    createToken(currToken, tokens, FLOAT, string(1, c), 1);
+                }
+                break;
+            case '.':
+                if (currToken.type != FLOAT || currToken.text.find(".") != string::npos) {
+                    createToken(currToken, tokens, OTHER, "error", 1);
+                    return tokens;
+                }
+                else {
+                    currToken.text += c;
+                    currToken.length++;
+                    currToken.columnNumber = tokens.back().columnNumber; 
+                    tokens.pop_back();
+                    createToken(currToken, tokens, DOT, currToken.text, currToken.length);
+                }
+                break;
+            
+            // handle unknown tokens (syntax error)
+            default:
+                if (!isspace(c)) {
+                    createToken(currToken, tokens, OTHER, string(1, c), 1);
+                    return tokens;
+                }
+        }
+    }
+
+    // post-processing
+    if (tokens.back().type == DOT) {
+        createToken(currToken, tokens, OTHER, "error", 1);
+    }
+    else if  (tokens.back().type != TokenType::END) {
+        createToken(currToken, tokens, END, "END", 1);
+    }
+
+    return tokens;
+}
+
+void printTokens(vector<Token> &tokens) {
+    if (tokens.back().type == TokenType::OTHER) {
+        cout << "Syntax error on line " << tokens.back().lineNumber << " column " 
+             << tokens.back().columnNumber << "." << endl;
+        exit(1);
+    }
+
+    for (const Token& token : tokens) {
+        cout << setw(4) << token.lineNumber << setw(5) << token.columnNumber << "  " << token.text << endl;
+    }
+}
+
+
--- src/parse.cpp
+++ src/parse.cpp
@@ -1,277 +1,127 @@
-#include "lib/parse.hpp"
-#include "lib/token.hpp"
-#include "lib/lex.hpp"
-#include <iomanip>
-#include <iostream>
-#include <algorithm>
-#include <vector>
-#include <string>
-#include <memory>
+# include "lib/parse.hpp"
+# include <iostream>
 
 using namespace std;
 
-class Node
-{
-public:
-    virtual ~Node() = default;
-};
-
-class OpNode : public Node
-{
-public:
-    TokenType op;
-    vector<unique_ptr<Node>> children;
-
-    OpNode(TokenType op) : op(op) {}
-};
-
-class NumNode : public Node
-{
-public:
-    double value;
-
-    NumNode(double value) : value(value) {}
-};
-
-// Parser
-class Parser
-{
-public:
-    unique_ptr<Node> parse(const vector<Token> &tokens);
-
-private:
-    const vector<Token> *tokens;
-    size_t current = 0;
-
-    unique_ptr<Node> expression();
-    unique_ptr<Node> term();
-    unique_ptr<Node> factor();
-    bool match(const vector<TokenType> &types);
-    const Token &advance();
-    const Token &peek();
-    const Token &previous();
-    bool isAtEnd();
-    void error(const Token &token, const string &message);
-};
-
-unique_ptr<Node> Parser::parse(const vector<Token> &tokens)
-{
-    this->tokens = &tokens;
-    return expression();
+AST::AST(const vector<Token>& tokens) {
+    int index = 0;
+    root = buildTree(tokens, index, tokens.size() - 1);
 }
 
-unique_ptr<Node> Parser::expression()
-{
-    unique_ptr<Node> node = term();
-
-    while (match({PLUS, MINUS}))
-    {
-        Token oper = previous();
-        unique_ptr<Node> right = term();
-        unique_ptr<OpNode> opNode = make_unique<OpNode>(oper.type);
-        opNode->children.push_back(move(node));
-        opNode->children.push_back(move(right));
-        node = move(opNode);
+Node* AST::buildTree(const vector<Token>& tokens, int index, int eindex) {
+    if (index >= tokens.size()) {
+        std::cerr << "Error: Invalid S expression. Unexpected end of input." << std::endl;
+        return nullptr;
     }
 
-    return node;
-}
-
-unique_ptr<Node> Parser::term()
-{
-    unique_ptr<Node> node = factor();
-
-    while (match({TIMES, DIVIDES}))
-    {
-        Token oper = previous();
-        unique_ptr<Node> right = factor();
-        unique_ptr<OpNode> opNode = make_unique<OpNode>(oper.type);
-        opNode->children.push_back(move(node));
-        opNode->children.push_back(move(right));
-        node = move(opNode);
-    }
-
-    return node;
-}
-
-unique_ptr<Node> Parser::factor()
-{
-    if (match({FLOAT}))
-    {
-        return make_unique<NumNode>(stod(previous().text));
-    }
-    if (match({LEFT_PAREN}))
-    {
-        unique_ptr<Node> node = expression();
-        if (!match({RIGHT_PAREN}))
-        {
-            error(peek(), "Expect ')' after expression.");
-        }
+    Token token = tokens[index];
+    
+    // If the current token is a float -> it is a leaf node, end recursion
+    if (token.type == FLOAT) {
+        Node* node = new Node();
+        node->token = token;
         return node;
     }
-    error(peek(), "Expect expression.");
-    return nullptr; // This will never be hit, but added to avoid compiler warning.
-}
-
-bool Parser::match(const vector<TokenType> &types)
-{
-    for (const auto &type : types)
-    {
-        if (isAtEnd())
-            return false;
-        if (peek().type == type)
-        {
-            advance();
-            return true;
+    // If the current token is a left parenthesis -> nested expression, recurse
+    else if (token.type == LEFT_PAREN) {
+        index += 1;
+        // Expression must start with an operator
+        if (tokens[index].type != PLUS && tokens[index].type != MINUS && tokens[index].type != TIMES && tokens[index].type != DIVIDES) {
+            std::cerr << "Error: Invalid S expression. Unexpected token '" << tokens[index].text << "'" << std::endl;
+            return nullptr;
         }
+        // Expression must end with a right parenthesis
+        if (tokens[eindex].type != RIGHT_PAREN) {
+            std::cerr << "Error: Invalid S expression. Missing or misplaced right parenthesis." << std::endl;
+            return nullptr;
+        }
+        // Create the root node of this subtree (operator)
+        Node* node = new Node();
+        // Recursively build the children for the root
+        while (index < eindex) {
+            // Fix this recursion so that tokens are not added multiple times and left and right parenthesis not added
+            Node* child = buildTree(tokens, index, eindex);
+            if (child) {
+                node->children.push_back(child);
+            }
+        }
+
+        if (tokens[index].type != RIGHT_PAREN) {
+            std::cerr << "Error: Invalid S expression. Missing or misplaced right parenthesis." << std::endl;
+            delete node;
+            return nullptr;
+        }
+
+        index++; // Move to the next token after the right parenthesis
+        return node;
+    } else {
+        std::cerr << "Error: Invalid S expression. Unexpected token '" << token.text << "'" << std::endl;
+        return nullptr;
     }
-    return false;
 }
 
-const Token &Parser::advance()
-{
-    if (!isAtEnd())
-        current++;
-    return previous();
+double AST::evaluateAST() {
+    if (!root) {
+        return 0;
+    }
+    return evaluate(root);
 }
 
-const Token &Parser::peek()
-{
-    return (*tokens)[current];
-}
-
-const Token &Parser::previous()
-{
-    return (*tokens)[current - 1];
-}
-
-bool Parser::isAtEnd()
-{
-    return current >= tokens->size() || peek().type == END;
-}
-
-void Parser::error(const Token &token, const string &message)
-{
-    cerr << "Unexpected token at line " << token.lineNumber << " column " << token.columnNumber << ": " << token.text << endl;
-    exit(2); // Exit with error code 2 as instructed
-}
-
-// Print in infix form
-void printInfix(Node *node)
-{
-    if (OpNode *op = dynamic_cast<OpNode *>(node))
-    {
-        cout << "(";
-        for (size_t i = 0; i < op->children.size(); ++i)
-        {
-            printInfix(op->children[i].get());
-            if (i < op->children.size() - 1)
-            {
-                switch (op->op)
-                {
-                case PLUS:
-                    cout << " + ";
-                    break;
-                case MINUS:
-                    cout << " - ";
-                    break;
-                case TIMES:
-                    cout << " * ";
-                    break;
-                case DIVIDES:
-                    cout << " / ";
-                    break;
-                default:
-                    break; // Error case, or not an operator.
+double AST::evaluate(Node *node) {
+    if (node->token.type == FLOAT) {
+        return stoi(node->token.text);
+    } else if (node->children.size() == 0) {
+        cerr << "Error: Invalid AST node." << endl;
+        return 0; // Or handle the error case as appropriate for your application
+    } else {
+        int result = evaluate(node->children[0]);
+        for (size_t i = 1; i < node->children.size(); i++) {
+            Token opToken = node->token;
+            if (opToken.type == PLUS) {
+                result += evaluate(node->children[i]);
+            } else if (opToken.type == MINUS) {
+                result -= evaluate(node->children[i]);
+            } else if (opToken.type == TIMES) {
+                result *= evaluate(node->children[i]);
+            } else if (opToken.type == DIVIDES) {
+                int denominator = evaluate(node->children[i]);
+                if (denominator != 0) {
+                    result /= denominator;
+                } else {
+                    cerr << "Error: Division by zero." << endl;
+                    return 0; // Or handle the error case as appropriate for your application
                 }
-            }
-        }
-        cout << ")";
-    }
-    else if (NumNode *num = dynamic_cast<NumNode *>(node))
-    {
-        cout << num->value;
-    }
-}
-
-double evaluate(Node *node)
-{
-    if (OpNode *op = dynamic_cast<OpNode *>(node))
-    {
-        double result = evaluate(op->children[0].get());
-        for (size_t i = 1; i < op->children.size(); ++i)
-        {
-            if (op->op == '+')
-            {
-                result += evaluate(op->children[i].get());
-            }
-            else if (op->op == '-')
-            {
-                result -= evaluate(op->children[i].get());
-            }
-            else if (op->op == '*')
-            {
-                result *= evaluate(op->children[i].get());
-            }
-            else if (op->op == '/')
-            {
-                double divisor = evaluate(op->children[i].get());
-                if (divisor == 0.0)
-                    throw runtime_error("division by zero");
-                result /= divisor;
             }
         }
         return result;
     }
-    else if (NumNode *num = dynamic_cast<NumNode *>(node))
-    {
-        return num->value;
-    }
-
-    throw runtime_error("Unknown node");
+}
+Node* AST::getRoot() const {
+    return root;
 }
 
-// Main Function
-int main(int argc, const char **argv)
-{
+void AST::printInfix(){
+}
+
+
+int main(int argc, const char** argv) {
     string input;
     string text;
-    vector<Token> tokens; // Assume this is filled with tokens after calling the lexer
-    // ... Lexer call to fill the tokens ...
-    while (getline(cin, input))
-    {
-        text += input;
-        if (!cin.eof())
-        {
-            text += '\n';
-        }
-    }
+    vector<Token> tokens;
 
+    // while (getline(cin, input)) {
+    //     text += input;
+    //     if (!cin.eof()) {
+    //         text += '\n';
+    //     }
+    // }
+
+    text = "(+(-2 4.444 ) 32(* 5 13.45))";
     tokens = readTokens(text);
-
-    Parser parser;
-    try
-    {
-        auto ast = parser.parse(tokens);
-        printInfix(ast.get());
-        cout << endl;
-        cout << evaluate(ast.get()) << endl;
-    }
-    catch (const runtime_error &e)
-    {
-        cerr << e.what() << endl;
-        return 3; // Runtime error
-    }
-    catch (const exception &e)
-    {
-        cerr << e.what() << endl;
-        return 2; // Parse error
-    }
-
-    return 0;
-}
-
-int main(int argc, const char **argv)
-{
+    printTokens(tokens);
+    
+    AST ast(tokens);
+    cout << ast.evaluateAST() << endl;
+    
     return 0;
 }
