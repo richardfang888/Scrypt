bfb528ac847b3cb063ade2a9ce28e90c165a1a3b
Richard Fang <richardfang@ucsb.edu>
2023-10-15 18:55:46 -0700

    fix case trailing decimal

--- src/lex.cpp
+++ src/lex.cpp
@@ -23,7 +23,7 @@ vector<Token> readTokens(string &input) {
 
     for (char c : input) {
         if (currToken.type == DOT && !(c >= '0' && c <= '9')) {\
-            currToken.columnNumber += tokens.back().length; 
+            currToken.columnNumber = tokens.back().columnNumber + tokens.back().length; 
             createToken(currToken, tokens, OTHER, string(1, c), 1);
             return tokens;
         }
