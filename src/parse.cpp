9f701e8ec5b77938ea449943f5a182c0450e3a30
Victor Prchlik <vprchlik@ucsb.edu>
2023-10-18 16:31:13 -0700

    test

--- src/parse.cpp
+++ src/parse.cpp
@@ -65,13 +65,6 @@ Node *AST::makeTree(const vector<Token> &tokens, int &index, int eindex)
             node->children.push_back(makeTree(tokens, index, eindex));
         }
 
-        if (node->children.empty() || (node->children.size() == 1 && node->children[0]->token.type != FLOAT))
-        {
-            printErrorTwo(tokens[index - 1]);
-            deleteNode(node);
-            return nullptr;
-        }
-
         if (index < eindex && tokens[index].type == RIGHT_PAREN)
         {
             index++;
