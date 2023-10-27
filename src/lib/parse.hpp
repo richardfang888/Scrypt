6948defbfffd9e0904898664faa6044f4fe3a6a9
Aaron Drumm <adrumm@cs.ucsb.edu>
2023-10-26 17:12:32 -0700

    Removed pointers from parse

--- src/lib/parse.hpp
+++ src/lib/parse.hpp
@@ -1,29 +1,30 @@
 #include "lex.hpp"
+#include <unordered_map>
 
 struct Node
 {
     Token token;
-    vector<Node *> children;
+    vector<Node> children;
 };
 
 class AST
 {
 public:
-    AST(const vector<Token> &tokens);
+    AST(const vector<Token> &tokens, int &index);
     ~AST();
 
-    double evaluateAST();
-    Node *getRoot() const;
+    double evaluateAST(std::unordered_map<std::string, double>& variables);
+    Node getRoot() const;
     void printInfix() const;
+    void printAST(Node node, int depth);
 
 private:
-    Node *root;
+    Node root;
 
-    double evaluate(Node *root) const;
-    Node *makeTree(const vector<Token> &tokens, int &index, int eindex);
-    void checkTree(Node *node, int childNum, int totalChildren, TokenType OPERATOR) const;
-    void deleteNode(Node *node);
-    void printInfix(const Node *node) const;
+    double evaluate(Node root, std::unordered_map<std::string, double>& variables) const;
+    Node makeTree(const vector<Token> &tokens, int &index);
+    void checkTree(Node node, int childNum, int totalChildren, TokenType OPERATOR) const;
+    void printInfix(const Node node) const;
 };
 
 void printErrorTwo(const Token &token);
--- src/parse.cpp
+++ src/parse.cpp
@@ -3,60 +3,59 @@
 
 using namespace std;
 
-AST::AST(const vector<Token> &tokens)
+AST::AST(const vector<Token> &tokens, int &index)
 {
     if (tokens.empty())
     {
         printErrorTwo(Token{END, "", 0, 1, 1});
         return;
     }
-    int index = 0;
-    root = makeTree(tokens, index, tokens.size() - 1);
+    root = makeTree(tokens, index);
+
+    printAST(root, 0);
+
+    cout << endl;
+
     checkTree(root, 0, 0, OTHER);
     if (index != static_cast<int>(tokens.size()) - 1)
     {
-        deleteNode(root);
         printErrorTwo(tokens[index]);
         return;
     }
 }
+void AST::printAST(Node node, int depth) {
+    // Print the value of the current node
+    cout << node.token.text << "|" << depth << "|||";
 
-AST::~AST()
-{
-    deleteNode(root);
-}
-
-void AST::deleteNode(Node *node)
-{
-    if (node)
+    // Recursively print the left and right subtrees
+    for (Node child : node.children)
     {
-        for (Node *child : node->children)
-        {
-            deleteNode(child);
-        }
-        delete node;
+        printAST(child, depth + 1);
     }
 }
 
+AST::~AST()
+{ }
+
 // Recursively creates an AST from a list of tokens,
 // checks for if there are formatting errors on converted S expression
-Node *AST::makeTree(const vector<Token> &tokens, int &index, int eindex)
+Node AST::makeTree(const vector<Token> &tokens, int &index)
 {
     Token token = tokens[index];
-
+    cout << "got here" << endl;
     // Base case: if the token is a FLOAT, simply return a new node with that token.
     if (token.type == FLOAT)
     {
-        Node *node = new Node();
-        node->token = token;
+        Node node;
+        node.token = token;
         index++;
         return node;
     }
 
     if (token.type == IDENTIFIER)
     {
-        Node *node = new Node();
-        node->token = token;
+        Node node;
+        node.token = token;
         index++;
         return node;
     }
@@ -64,41 +63,35 @@ Node *AST::makeTree(const vector<Token> &tokens, int &index, int eindex)
     // If the token is a LEFT_PAREN, then a new operation is starting.
     else if (token.type == LEFT_PAREN)
     {
-        Node *node = new Node();
+        Node node;
         index++;
 
         // Check if the token after LEFT_PAREN is a valid operation. If not, throw an error.
-        if (index > eindex || (tokens[index].type != PLUS && tokens[index].type != MINUS &&
+        if ( index > tokens.size() - 1||(tokens[index].type != PLUS && tokens[index].type != MINUS &&
                                tokens[index].type != TIMES && tokens[index].type != DIVIDES && tokens[index].type != ASSIGN))
         {
             printErrorTwo(tokens[index]);
-            deleteNode(node);
-            return nullptr;
         }
-        node->token = tokens[index++];
+        node.token = tokens[index++];
 
         // Error checking for an empty operation
         if (tokens[index].type == RIGHT_PAREN)
         {
             printErrorTwo(tokens[index]);
-            deleteNode(node);
-            return nullptr;
         }
 
         // While there are more tokens inside the parentheses, recursively create child nodes.
-        while (index < eindex && tokens[index].type != RIGHT_PAREN)
+        while (index < tokens.size() - 1 && tokens[index].type != RIGHT_PAREN)
         {
-            node->children.push_back(makeTree(tokens, index, eindex));
+            node.children.push_back(makeTree(tokens, index));
         }
 
         // Error handling for incorrect expressions (like "(+ 1 2" without the closing parenthesis)
-        if (node->children.empty() || (node->children.size() == 1 && node->children[0]->token.type != FLOAT))
+        if (node.children.empty() || (node.children.size() == 1 && node.children[0].token.type != FLOAT))
         {
             printErrorTwo(tokens[index - 1]);
-            deleteNode(node);
-            return nullptr;
         }
-        else if (index < eindex && tokens[index].type == RIGHT_PAREN)
+        else if (index < tokens.size() - 1 && tokens[index].type == RIGHT_PAREN)
         {
             index++;
         }
@@ -106,78 +99,81 @@ Node *AST::makeTree(const vector<Token> &tokens, int &index, int eindex)
         else
         {
             printErrorTwo(tokens[index]);
-            return nullptr;
         }
         return node;
     }
     else
     {
         printErrorTwo(token);
-        return nullptr;
     }
+    throw 1;
 }
 
-void AST::checkTree(Node *node, int childNum, int totalChildren, TokenType OPERATOR) const
+void AST::checkTree(Node node, int childNum, int totalChildren, TokenType OPERATOR) const
 {
     if(OPERATOR == ASSIGN){
-        if(childNum != totalChildren-1 && node->token.type != IDENTIFIER){
-            printErrorTwo(node->token);
+        if(childNum != totalChildren-1 && node.token.type != IDENTIFIER){
+            printErrorTwo(node.token);
         }
     }
     long unsigned int i = 0;
-    while (i < node->children.size()){
-        checkTree(node->children[i], i, node->children.size(), node->token.type);
+    while (i < node.children.size()){
+        checkTree(node.children[i], i, node.children.size(), node.token.type);
         i++;
     }
 }
 
-double AST::evaluateAST()
+double AST::evaluateAST(std::unordered_map<std::string, double>& variables)
 {
-    if (!root)
-    {
-        return 0;
-    }
-    return evaluate(root);
+    return evaluate(root, variables);
 }
 
 // Evaluates the given AST node and returns the result of the original expression.
-double AST::evaluate(Node *node) const
+double AST::evaluate(Node node, std::unordered_map<std::string, double>& variables) const
 {
-    // If the node holds a FLOAT token, simply return its value.
-    if (node->token.type == FLOAT)
+    if (node.token.type == FLOAT)
     {
-        return stod(node->token.text);
+        return stod(node.token.text);
+    }
+    else if(node.token.type == IDENTIFIER){
+        if (variables.count(node.token.text) > 0){
+            return variables.at(node.token.text);
+        }
+        else{
+            cout << "Runtime error: unknown identifier " << node.token.text << endl;
+        }
     }
 
+
     // If the node does not have any children, throw an error.
-    else if (node->children.size() == 0)
+    else if (node.children.size() == 0)
     {
-        printErrorTwo(node->token);
+        printErrorTwo(node.token);
         return 2;
     }
     else
     {
         // Iterate over the rest of the children to apply the operation.
-        double result = evaluate(node->children[0]);
-        for (size_t i = 1; i < node->children.size(); i++)
+        double result = evaluate(node.children[0], variables);
+        for (size_t i = 1; i < node.children.size(); i++)
         {
-            Token opToken = node->token;
+            Token opToken = node.token;
             if (opToken.type == PLUS)
             {
-                result += evaluate(node->children[i]);
+                result += evaluate(node.children[i], variables);
             }
             else if (opToken.type == MINUS)
             {
-                result -= evaluate(node->children[i]);
+                result -= evaluate(node.children[i], variables);
             }
             else if (opToken.type == TIMES)
             {
-                result *= evaluate(node->children[i]);
+                result = evaluate(node.children[i], variables);
             }
             else if (opToken.type == DIVIDES)
             {
                 // Check for division by zero.
-                double denominator = evaluate(node->children[i]);
+                double denominator = evaluate(node.children[i], variables);
                 if (denominator != 0)
                 {
                     result /= denominator;
@@ -190,7 +186,10 @@ double AST::evaluate(Node *node) const
             }
             else if (opToken.type == ASSIGN)
             {
-                result = evaluate(node->children[i]);
+                result = evaluate(node.children[node.children.size()-1], variables);
+                for(int i = node.children.size()-2; i >= 0; i--){
+                    variables.at(node.children[i].token.text) = result;
+                }
             }
             else
             {
@@ -201,63 +200,61 @@ double AST::evaluate(Node *node) const
         }
         return result;
     }
+    return 2;
+
 }
 
-Node *AST::getRoot() const
+Node AST::getRoot() const
 {
     return root;
 }
 
 void AST::printInfix() const
 {
-    if (root && (root->token.type != FLOAT && root->token.type != IDENTIFIER))
+    if ((root.token.type != FLOAT && root.token.type != IDENTIFIER))
         cout << "(";
 
     printInfix(root);
-    if (root && (root->token.type != FLOAT && root->token.type != IDENTIFIER))
+    if ((root.token.type != FLOAT && root.token.type != IDENTIFIER))
         cout << ")";
 
     cout << endl;
 }
 
 // Prints the infix notation of a given AST.
-void AST::printInfix(const Node *node) const
+void AST::printInfix(const Node node) const
 {
-    if (!node)
+    if (node.token.type == FLOAT)
     {
-        return;
-    }
-    else if (node->token.type == FLOAT)
-    {
-        double val = stod(node->token.text);
+        double val = stod(node.token.text);
         if (val == static_cast<int>(val))
             cout << static_cast<int>(val);
         else
-            cout << node->token.text;
+            cout << node.token.text;
     }
-    else if (node->token.type == IDENTIFIER)
+    else if (node.token.type == IDENTIFIER)
     {
-        cout << node->token.text;
+        cout << node.token.text;
     }
     else
     {
         bool isFirst = true;
-        for (const auto &child : node->children)
+        for (const auto &child : node.children)
         {
             if (!isFirst)
             {
-                cout << " " << node->token.text << " ";
+                cout << " " << node.token.text << " ";
             }
             else
             {
                 isFirst = false;
             }
-            if (child->token.type != FLOAT && child->token.type != IDENTIFIER)
+            if (child.token.type != FLOAT && child.token.type != IDENTIFIER)
             {
                 cout << "(";
             }
             printInfix(child);
-            if (child->token.type != FLOAT && child->token.type != IDENTIFIER)
+            if (child.token.type != FLOAT && child.token.type != IDENTIFIER)
             {
                 cout << ")";
             }
@@ -289,13 +286,45 @@ int main(int argc, const char **argv)
         }
     }
 
-    //text = "(= b 2 (+ 2 3 4))";
+    // text = "(= b c (+ 6 3 4))";
+    // text = "(+ 4 5 6)";
 
     tokens = readTokens(text);
     checkLexErrors(tokens);
+    int index1 = 0;
+    AST ast1(tokens, index1);
+    ast1.printInfix();
 
-    AST ast(tokens);
-    ast.printInfix();
+    // int index = 0;
+    // vector<AST> trees;
+    // //std::unordered_map<std::string, double> variables;
+
+    // AST ast(tokens, index);
+    // trees.push_back(ast);
+    // trees[0].printInfix();
+    // cout << "after infix" << endl;
+
+
+    // while(tokens[index].type != END){
+    //     cout << "length is " << tokens.size() << endl;
+    //     AST ast(tokens, index);
+    //     trees.push_back(ast);
+    //     cout << "index is " << index << endl;
+    // }
+
+    // cout << trees.size() << endl;
+    // trees[0].printInfix();
+    // cout << "HAHAHAHAHAHAaa" << endl;
+
+    
+    // for(auto tree : trees){
+    //     //printAST(tree.getRoot());
+    //     cout << "HERE" << endl;
+    //     tree.printInfix();
+    //     cout << "HERE HERE HERE" << endl;
+    //     //cout << tree.evaluateAST(variables) << endl;
+    // }
+    //ast.printInfix();
     //cout << ast.evaluateAST() << endl;
 
     return 0;
